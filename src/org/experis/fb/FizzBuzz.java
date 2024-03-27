b49817677e199f3b002b6cd015602ca23be987e5
ValerioCarbone <valeriocarbone10@gmail.com>
2024-03-27 15:13:56 +0100

    Third exercise

---
+++ src/org/experis/fb/FizzBuzz.java
@@ -0,0 +1,21 @@
+package org.experis.fb;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++){
+            int currentNum = i;
+
+            if (currentNum % 3 == 0 && currentNum % 5 == 0){
+                System.out.println("FizzBuzz");
+            }else if (currentNum % 3 == 0){
+                System.out.println("Fizz");
+            } else if (currentNum % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(currentNum);
+            }
+
+        }
+
+    }
+}
